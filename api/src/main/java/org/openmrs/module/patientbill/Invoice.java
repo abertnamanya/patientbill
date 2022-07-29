5f68863347b4a2fdd999f159d2fc5e1a4925bf6f
abertnamanya <abertnamanya@gmail.com>
2022-07-29 10:43:00 +0300

    Created Invoice and Invoice Item DataModel

---
+++ api/src/main/java/org/openmrs/module/patientbill/Invoice.java
@@ -0,0 +1,85 @@
+package org.openmrs.module.patientbill;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import java.util.Set;
+
+import org.openmrs.BaseOpenmrsData;
+import org.openmrs.Patient;
+import org.openmrs.module.patientbill.api.InvoiceItem;
+
+@Entity
+@Table(name = "invoice")
+public class Invoice extends BaseOpenmrsData {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "invoice_id", nullable = false)
+	private Integer InvoiceId;
+	
+	//	@GeneratedValue(strategy = GenerationType.AUTO, generator = "generator")
+	//	@SequenceGenerator(name = "generator", sequenceName = "idgen_seq_id_gen")
+	@Column(name = "invoice_no")
+	private Integer InvoiceNo;
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "patient_id")
+	private Patient patient;
+	
+	@OneToMany(mappedBy = "invoice")
+	private Set<InvoiceItem> invoiceItems;
+	
+	public Invoice() {
+	}
+	
+	public Integer getInvoiceId() {
+		return InvoiceId;
+	}
+	
+	public void setInvoiceId(Integer invoiceId) {
+		this.InvoiceId = invoiceId;
+	}
+	
+	@Override
+	public Integer getId() {
+		return getInvoiceId();
+	}
+	
+	@Override
+	public void setId(Integer integer) {
+		setInvoiceId(integer);
+	}
+	
+	public Integer getInvoiceNo() {
+		return InvoiceNo;
+	}
+	
+	public void setInvoiceNo(Integer invoiceNo) {
+		InvoiceNo = invoiceNo;
+	}
+	
+	public Patient getPatient() {
+		return patient;
+	}
+	
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+	
+	public Set<InvoiceItem> getInvoiceItems() {
+		return invoiceItems;
+	}
+	
+	public void setInvoiceItems(Set<InvoiceItem> invoiceItems) {
+		this.invoiceItems = invoiceItems;
+	}
+}
---
+++ api/src/main/java/org/openmrs/module/patientbill/api/InvoiceItem.java
@@ -0,0 +1,68 @@
+package org.openmrs.module.patientbill.api;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.openmrs.BaseOpenmrsObject;
+import org.openmrs.module.patientbill.Invoice;
+import org.openmrs.module.patientbill.ServiceCatalog;
+
+@Entity
+@Table(name = "invoice_item")
+public class InvoiceItem extends BaseOpenmrsObject {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "invoice_item_id", nullable = false)
+	private Integer invoiceItemId;
+	
+	@ManyToOne
+	@Column(name = "invoice_id")
+	private Invoice invoice;
+	
+	@ManyToOne
+	@Column(name = "service_catalog_id")
+	private ServiceCatalog serviceCatalog;
+	
+	public InvoiceItem() {
+	}
+	
+	public Integer getInvoiceItemId() {
+		return invoiceItemId;
+	}
+	
+	public void setInvoiceItemId(Integer invoiceItemId) {
+		this.invoiceItemId = invoiceItemId;
+	}
+	
+	public Invoice getInvoice() {
+		return invoice;
+	}
+	
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}
+	
+	public ServiceCatalog getServiceCatalog() {
+		return serviceCatalog;
+	}
+	
+	public void setServiceCatalog(ServiceCatalog serviceCatalog) {
+		this.serviceCatalog = serviceCatalog;
+	}
+	
+	@Override
+	public Integer getId() {
+		return getInvoiceItemId();
+	}
+	
+	@Override
+	public void setId(Integer integer) {
+		setInvoiceItemId(integer);
+	}
+}
